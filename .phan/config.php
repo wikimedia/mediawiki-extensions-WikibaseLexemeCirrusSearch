8a8f7fe5d91df713b48fd2487d6be35055671871
James D. Forrester <jforrester@wikimedia.org>
2019-05-19 13:40:07 +0200

    build: Upgrade mediawiki/mediawiki-phan-config from 0.5.0 to 0.6.0
    Change-Id: I1575754bebdeb36859aef2805c56b00657644960

--- .phan/config.php
+++ .phan/config.php
@@ -1,4 +1,5 @@
 <?php
+
 $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
 
 $cfg['directory_list'] = array_merge(
@@ -29,4 +30,5 @@ $cfg['exclude_analysis_directory_list'] = array_merge(
 		'../../extensions/WikibaseCirrusSearch',
 	]
 );
+
 return $cfg;
