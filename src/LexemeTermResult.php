1dd9f632ca9804609605e4924982e42ac338bc98
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2019-12-22 12:34:50 +0000

    build: Updating mediawiki/mediawiki-phan-config to 0.9.0
    Depends-On: I033334a765127113fe800deef84437cc950a08fc
    Change-Id: Ibe5448852b61c7e9a904bbde295a02626ed406a6

--- src/LexemeTermResult.php
+++ src/LexemeTermResult.php
@@ -159,6 +159,7 @@ class LexemeTermResult extends BaseResultsType {
 			} else {
 				// matched one of the forms
 				$matchedTermType = 'alias';
+				// @phan-suppress-next-line PhanTypePossiblyInvalidDimOffset
 				$matchedTerm = new Term( $lemmaCode, $highlight['lexeme_forms.representation'][0] );
 			}
 
