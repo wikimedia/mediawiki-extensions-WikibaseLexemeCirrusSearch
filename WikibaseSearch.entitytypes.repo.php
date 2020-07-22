f2095a2548f2dd6014dae9a8b1974f84432a54f7
David Causse <dcausse@wikimedia.org>
2020-07-22 09:16:03 +0200

    Combine DB lookups with elastic
    Should help to mitigate the lag on the search indices when users paste a lexeme
    or form ID
    
    Bug: T240328
    Change-Id: I8b229a049faf5bda701e5763cb89e43009315794

--- WikibaseSearch.entitytypes.repo.php
+++ WikibaseSearch.entitytypes.repo.php
@@ -2,17 +2,22 @@
 
 use MediaWiki\MediaWikiServices;
 use Wikibase\DataModel\Services\Lookup\InProcessCachingDataTypeLookup;
+use Wikibase\Lexeme\DataAccess\Store\NullLabelDescriptionLookup;
 use Wikibase\Lexeme\Search\Elastic\FormSearchEntity;
 use Wikibase\Lexeme\Search\Elastic\LexemeFieldDefinitions;
 use Wikibase\Lexeme\Search\Elastic\LexemeFullTextQueryBuilder;
 use Wikibase\Lexeme\Search\Elastic\LexemeSearchEntity;
+use Wikibase\Lib\EntityTypeDefinitions as Def;
 use Wikibase\Lib\SettingsArray;
+use Wikibase\Lib\Store\LanguageFallbackLabelDescriptionLookup;
+use Wikibase\Repo\Api\CombinedEntitySearchHelper;
+use Wikibase\Repo\Api\EntityIdSearchHelper;
 use Wikibase\Repo\WikibaseRepo;
 use Wikibase\Search\Elastic\Fields\StatementProviderFieldDefinitions;
 
 return [
 	'lexeme' => [
-		'search-field-definitions' => function ( array $languageCodes, SettingsArray $searchSettings ) {
+		Def::SEARCH_FIELD_DEFINITIONS => function ( array $languageCodes, SettingsArray $searchSettings ) {
 			$repo = WikibaseRepo::getDefaultInstance();
 			$config = MediaWikiServices::getInstance()->getMainConfig();
 			if ( $config->has( 'LexemeLanguageCodePropertyId' ) ) {
@@ -30,29 +35,50 @@ return [
 				$lcID ? $repo->getEntityIdParser()->parse( $lcID ) : null
 			);
 		},
-		'entity-search-callback' => function ( WebRequest $request ) {
+		Def::ENTITY_SEARCH_CALLBACK => function ( WebRequest $request ) {
 			$repo = WikibaseRepo::getDefaultInstance();
-
-			return new LexemeSearchEntity(
-					$repo->getEntityIdParser(),
-					$request,
-					$repo->getUserLanguage(),
-					$repo->getLanguageFallbackChainFactory(),
-					$repo->getPrefetchingTermLookup()
+			return new CombinedEntitySearchHelper(
+				[
+					new EntityIdSearchHelper(
+						$repo->getEntityLookup(),
+						$repo->getEntityIdParser(),
+						new LanguageFallbackLabelDescriptionLookup(
+							$repo->getTermLookup(),
+							$repo->getLanguageFallbackChainFactory()->newFromLanguage( $repo->getUserLanguage() )
+						),
+						$repo->getEntityTypeToRepositoryMapping()
+					),
+					new LexemeSearchEntity(
+						$repo->getEntityIdParser(),
+						$request,
+						$repo->getUserLanguage(),
+						$repo->getLanguageFallbackChainFactory(),
+						$repo->getPrefetchingTermLookup()
+					)
+				]
 			);
 		},
-		'fulltext-search-context' => LexemeFullTextQueryBuilder::CONTEXT_LEXEME_FULLTEXT,
+		Def::FULLTEXT_SEARCH_CONTEXT => LexemeFullTextQueryBuilder::CONTEXT_LEXEME_FULLTEXT,
 	],
 	'form' => [
-		'entity-search-callback' => function ( WebRequest $request ) {
+		Def::ENTITY_SEARCH_CALLBACK => function ( WebRequest $request ) {
 			$repo = WikibaseRepo::getDefaultInstance();
-
-			return new FormSearchEntity(
-					$repo->getEntityIdParser(),
-					$request,
-					$repo->getUserLanguage(),
-					$repo->getLanguageFallbackChainFactory(),
-					$repo->getPrefetchingTermLookup()
+			return new CombinedEntitySearchHelper(
+				[
+					new Wikibase\Repo\Api\EntityIdSearchHelper(
+						$repo->getEntityLookup(),
+						$repo->getEntityIdParser(),
+						new NullLabelDescriptionLookup(),
+						$repo->getEntityTypeToRepositoryMapping()
+					),
+					new FormSearchEntity(
+						$repo->getEntityIdParser(),
+						$request,
+						$repo->getUserLanguage(),
+						$repo->getLanguageFallbackChainFactory(),
+						$repo->getPrefetchingTermLookup()
+					),
+				]
 			);
 		},
 	],
